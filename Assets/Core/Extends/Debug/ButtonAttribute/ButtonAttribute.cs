792c486044247bc55bf488129ec8332bdb943e65
laoqiu <137441398@qq.com>
2019-06-13 17:19:11 +0800

    增加ButtonAttribute

---
+++ Assets/Core/Extends/Debug/ButtonAttribute/ButtonAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+public class ButtonAttribute : PropertyAttribute
+{
+    //按钮高度
+    public readonly float height = 16;
+    //绑定的函数
+    public readonly string[] funcNames = null;
+    //运行时是否显示
+    public bool showOnPlay = true;  //True:显示
+
+    public ButtonAttribute(params string[] funcNames)
+    {
+        this.funcNames = funcNames;
+    }
+
+    public ButtonAttribute(bool showOnPlay, float height, params string[] funcNames)
+    {
+        this.showOnPlay = showOnPlay;
+        this.height = height;
+        this.funcNames = funcNames;
+    }
+}
